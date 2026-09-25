e1682fb11cb37c44f1e537729dbbeaf3d21c8855
HEMANTVS05 <vshemant05@gmail.com>
2026-09-25 16:28:57 +0530

    untrack backend/ (maintained separately in startup-peravai-backend)

--- backend/src/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { authenticateUser } = require('../middleware/authMiddleware');
-const { getProfile, updateProfile } = require('../controllers/userController');
-
-router.get('/me',      authenticateUser, getProfile);
-router.patch('/me',    authenticateUser, updateProfile);
-
-module.exports = router;
